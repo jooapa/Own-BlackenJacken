cd84ee5f4ccef26358823d6cc54f31ba189875bf
jooapas <jooa.akonpelto@gmail.com>
2022-10-30 16:40:41 +0200

    Updated the BJ.JS now its not that cringe :alien:

--- TheGame/BJ.js
+++ TheGame/BJ.js
@@ -1,27 +1,3 @@
-/*
-Tee ohjelma jossa käyttäjälle arvotaan kaksi korttia ja 
-tietokone-vastustajalle kaksi korttia. Tämän jälkeen edetään siten, 
-että käyttäjä saa päättää haluaako nostaa lisää kortteja. 
-Hyödynnä korttien nostamisessa funktiota. 
-
-x
-Tietokone ei nosta lisää kortteja vaan jää lukemaan 
-jonka sai kahdesta ensimmäisestä kortista. Lähemmäs 21 päässyt pelaaja voittaa.
-x
-
-Kortteja nostetaan lisää yksi kerrallaan, niin kauan kuin käyttäjä haluaa 
-tai kunnes hänen korttiensa lukema ylittää 21, jolloin hän on hävinnyt.
- 
-Lopuksi tulostetaan voittaja ja korttien yhteenlasketut lukemat funktiota 
-hyödyntäen esim. seuraavasti:
-
-Voitit! Sinulla on 20, tietokoneella on 18.
-
-tai
-Hävisit! Sinulla on 22, tietokoneella on 12.
-
-Saat vähän lisää haastetta, kun teet tehtävän graafisena.
-*/
 CardPack = [
     ["1", new Image()],
     ["2", new Image()],
